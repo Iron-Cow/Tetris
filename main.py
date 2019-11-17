b0418e6f0d8417f8d7f05cd2b74cd061f3430387
Iron-Cow <uangeji@gmail.com>
2019-11-17 23:28:50 +0200

    Figure creation/movement/restrictions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from model import Block, Field
+from model import Block, Field, Figure
 from gui import field, draw_bricks, add_square, check_lines
 
 if __name__ == "__main__":
@@ -14,7 +14,16 @@ if __name__ == "__main__":
                                       screen.get_h() + screen.get_h_boarder()*2))
 
     # Block size initiating
-    block = Block(260, 60, 50, 50, (255, 255, 255), screen)
+    # block = Block(260, 60, 50, 50, (255, 255, 255), screen)
+    figure = Figure(surface=window,
+                    x=60,
+                    y=60,
+                    name="L",
+                    color=(255, 255, 255),
+                    config=[[0, 0, 0], [1, 0, 0], [1, 1, 1]],
+                    field=screen,
+                    block_field=block_field
+                    )
 
     clock = pygame.time.Clock()
     tick_counter = 0
@@ -33,33 +42,49 @@ if __name__ == "__main__":
             if event.type == pygame.KEYDOWN:
                 try:
                     if event.key == pygame.K_RIGHT:
-                        if not block_field[(block.get_y()-screen.get_h_boarder())//block.get_h()][(block.get_x()-screen.get_v_boarder())//block.get_w() + 1]:
-                            block.move_right()
+                        # if not block_field[(block.get_y()-screen.get_h_boarder())//block.get_h()][(block.get_x()-screen.get_v_boarder())//block.get_w() + 1]:
+                        #     block.move_right()
+                        figure.move_right()
 
                     elif event.key == pygame.K_LEFT:
-                        if not block_field[(block.get_y()-screen.get_h_boarder())//block.get_h()][(block.get_x()-screen.get_v_boarder())//block.get_w() - 1]:
-                            block.move_left()
+                        # if not block_field[(block.get_y()-screen.get_h_boarder())//block.get_h()][(block.get_x()-screen.get_v_boarder())//block.get_w() - 1]:
+                        #     block.move_left()
+                        figure.move_left()
+
+                    elif event.key == pygame.K_SPACE:
+                        figure = Figure(surface=window,
+                                        x=60,
+                                        y=60,
+                                        name="L",
+                                        color=(255, 255, 255),
+                                        config=[[0, 0, 0], [1, 0, 0], [1, 1, 1]],
+                                        field=screen,
+                                        block_field=block_field
+                                        )
+
                 except IndexError:
                     pass
 
         if tick_counter == 10:  # each 10th frame the movement down will be initiated
-            try:
-                # if block under current if empty - block can be dropped
-                if block_field[block.get_y() // block.get_h() + 1][block.get_x() // block.get_w()] == 0:
-                    block.drop_block()
-                # else: block reached filled block and should be marked as filled also + initiate new block
-                else:
-                    add_square(block_field, block)
-                    block = Block(260, 60, 50, 50, (255, 255, 255), screen)
-
-            except IndexError:  # block reached ground
-                add_square(block_field, block)
-                block = Block(260, 60, 50, 50, (255, 255, 255), screen)
+            # try:
+            #     # if block under current if empty - block can be dropped
+            #     if block_field[block.get_y() // block.get_h() + 1][block.get_x() // block.get_w()] == 0:
+            #         block.drop_block()
+            #     # else: block reached filled block and should be marked as filled also + initiate new block
+            #     else:
+            #         add_square(block_field, block)
+            #         block = Block(260, 60, 50, 50, (255, 255, 255), screen)
+            #
+            # except IndexError:  # block reached ground
+            #     add_square(block_field, block)
+            #     block = Block(260, 60, 50, 50, (255, 255, 255), screen)
+            figure.fall()
             tick_counter = 0
 
-        draw_bricks(block_field, window, f=screen, b=block)  # draw filled bricks
-        block.draw_block(window)  # draw current dropping brick
-        check_lines(block_field)  # check if fully filled line on the field and deletes it
+        # draw_bricks(block_field, window, f=screen, b=block)  # draw filled bricks
+        # block.draw_block(window)  # draw current dropping brick
+        # check_lines(block_field)  # check if fully filled line on the field and deletes it
+        figure.draw_figure()
 
         pygame.display.update()
 
--- model.py
+++ model.py
@@ -71,3 +71,71 @@ class Block:
     def move_right(self):
         if self.__x < self.__field.get_w() + self.__field.get_v_boarder() - self.__w:
             self.__x += self.__w
+
+
+class Figure:
+    """Multiblock figure like in real tetris"""
+    def __init__(self, surface, x: float, y: float,  name: str, config: list, color: tuple, field: Field,
+                 block_field, block_w: float = 50, block_h: float = 50, ):
+        self.__surface = surface
+        self.__x = x
+        self.__y = y
+        self.__name = name
+        self.__config = config
+        self.__color = color
+        self.__field = field
+        self.__block_w = block_w
+        self.__block_h = block_h
+        self.__block_field = block_field
+
+        """[[0, 0, 0],
+            [1, 0, 0], 
+            [1, 1, 1]]"""
+
+    def __generate_block_list(self) -> list:
+        """Generates list of Block objects"""
+        block_list = []
+        for i, row in enumerate(self.__config):
+            for j, el in enumerate(row):
+                if el:
+                    b = Block(x=self.__x + j * self.__block_w,
+                              y=self.__y + i * self.__block_h,
+                              w=self.__block_w,
+                              h=self.__block_h,
+                              color=self.__color,
+                              field=self.__field)
+                    block_list.append(b)
+        return block_list
+
+    def draw_figure(self) -> None:
+        for block in self.__generate_block_list():
+            block.draw_block(self.__surface)
+
+    def fall(self):
+        for block in self.__generate_block_list():
+            try:
+                # if block under current if empty - block can be dropped
+                if self.__block_field[block.get_y() // block.get_h() + 1][block.get_x() // block.get_w()] == 0:
+                    pass
+                # else: block reached filled block and should be marked as filled also + initiate new block
+                else:
+                    # add_square(self.__field, block)
+                    block = Block(260, 60, 50, 50, (255, 255, 255), self.__surface)
+
+            except IndexError:  # block reached ground
+                return
+        self.__y += self.__block_h
+            # block.draw_block(self.__surface)
+
+    def move_left(self) -> None:
+        for block in self.__generate_block_list():
+            if block.get_x() <= self.__field.get_v_boarder():
+                return
+        self.__x -= self.__block_w
+
+    def move_right(self) -> None:
+        for block in self.__generate_block_list():
+            if block.get_x() >= self.__field.get_w() + self.__field.get_v_boarder() - self.__block_w:
+                return
+        self.__x += self.__block_w
+
